da3911e9f5ba5b87a093caa2e67aff414f9426c3
Trebuchet1 <jc.kulczycki@student.uw.edu.pl>
2022-06-08 15:20:03 +0200

    adding more packages, reading config, reading file contents

--- main_code.py
+++ main_code.py
@@ -2,5 +2,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import os
+import glob
 
-files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+config = open('config.txt').read()
+print(config)
+files = sorted(glob.glob(os.path.join("data","*.csv")))
+print(files)
