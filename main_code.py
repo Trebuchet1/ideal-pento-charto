9a992ee2516ba0678bb79c93b40f7331584100e6
Trebuchet1 <jc.kulczycki@student.uw.edu.pl>
2022-06-12 20:44:22 +0200

    major update, the config file takes folder name and columns for the files in the folder, the program now creates a joined array with data specified from all the files and a header for that data, use freely and hopefully with ease

--- main_code.py
+++ main_code.py
@@ -4,7 +4,26 @@ import pandas as pd
 import os
 import glob
 
-config = open('config.txt').read()
-print(config)
-files = sorted(glob.glob(os.path.join("data","*.csv")))
-print(files)
+config = open('config.txt', 'r').read()
+print()
+folder = config.split('\n')[2].split('=')[1].strip() # NOTE: creates a list of files within a folder specified in config file
+from_col = int(config.split('\n')[5].split('=')[1])
+to_col = int(config.split('\n')[6].split('=')[1])
+
+cwd = os.getcwd() # NOTE: gets the current directory
+files = sorted(glob.glob(os.path.join(cwd, folder,"*.txt"))) # NOTE: creates a list of files in a directory we want
+print('Used files:\n', ',\n'.join([os.path.basename(x) for x in files]))
+
+
+header = []
+for x in range(0, len(files)):
+    t_file = open(files[x], 'r')
+    print('loaded')
+    t_header = t_file.readline().split()
+    t_array = np.loadtxt(t_file, skiprows=1)
+    data = t_array[0,:]
+    for x in range(from_col, to_col):
+        header.append(t_header[x])
+        np.append(data, t_array[x, :])
+# NOTE: header is a list of column names
+# NOTE: data is a numpy array of data coresponding to header
