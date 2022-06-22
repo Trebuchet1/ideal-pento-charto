e07cb6b0e427287acf53fef90cd19e58be14383a
Trebuchet1 <jc.kulczycki@student.uw.edu.pl>
2022-06-22 21:23:18 +0200

    preparations for 'specific file' feature

--- main_code.py
+++ main_code.py
@@ -3,18 +3,23 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import os
 import glob
-
+# NOTE: gets the current directory
+cwd = os.getcwd()
 config = open('config.txt', 'r').read()
 # NOTE: creates a list of files within a folder specified in config file
 folder = config.split('\n')[2].split('=')[1].strip()
 from_col = int(config.split('\n')[5].split('=')[1])
 to_col = int(config.split('\n')[6].split('=')[1])
+files_list = config.split('\n')[9].split('=')[1]
 
-# NOTE: gets the current directory
-cwd = os.getcwd()
-# NOTE: creates a list of files in a directory we want
-files = sorted(glob.glob(os.path.join(cwd, folder,"*.txt")))
-print('Used files:\n', ',\n'.join([os.path.basename(x) for x in files]))
+if files_list.strip():
+    # NOTE: creates a list of specified files if there is anything in the config
+    files = [os.path.join(cwd, folder,x.strip()) for x in files_list.split(',')]
+    print('Using files:\n', '\n'.join([os.path.basename(x) for x in files]))
+else:
+    # NOTE: creates a list of all files in a directory we want
+    files = sorted(glob.glob(os.path.join(cwd, folder,"*.txt")))
+    print('Using files:\n', ',\n'.join([os.path.basename(x) for x in files]))
 
 header = []
 for x in range(0, len(files)):
@@ -39,7 +44,7 @@ def D(x,y):
         xtemp = (x[i+1]+x[i])/2
         xprime = np.append(xprime,xtemp)
     return xprime, yprime
-
+#loops for making data
 axx = data[0, :]
 for x in range(1, (len(header)+1)):
     axy = data[x, :]
@@ -50,9 +55,10 @@ for x in range(1, (len(header)+1)):
     xprime, yprime = D(axx,axy)
     xprime, yprime = D(xprime, yprime)
     ax.plot(xprime,yprime,label = ("2nd derivative of " + header[x-1]))
-
+#making labels
 plt.xlabel(header[0])
 plt.ylabel(":)")
+
 plt.legend()
 
 plt.show()
